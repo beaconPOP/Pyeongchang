371ad7c5adf221c32deca520ae083bf41e892867
gwmail <gwwork01@gmail.com>
2017-08-22 21:08:16 +0900

    axz

--- app/src/main/java/com/example/becomebeacon/beaconlocker/MainActivity.java
+++ app/src/main/java/com/example/becomebeacon/beaconlocker/MainActivity.java
@@ -212,7 +212,7 @@ public class MainActivity extends AppCompatActivity
 
         try {
             super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main_slide);
+            setContentView(R.layout.activity_main_lostitem);
 
             mainProgressDialog = new ProgressDialog(this);
             mainProgressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
